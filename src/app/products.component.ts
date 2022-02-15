350300997793627249bb8feef89331d10eb0bb34
beataj <beata@emc2wave.com>
2022-02-14 20:45:54 -0800

    add property dynamically

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -6,5 +6,12 @@ import { Component } from "@angular/core";
 
 })
 export class ProductsComponent {
+  productName = 'A Book';
+
+  constructor() {
+    setTimeout(() => {
+      this.productName = 'A Tree';
+    }, 3000);
+  }
 
 }
