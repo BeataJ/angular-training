583c916f59e37cd059581c49ffe62f3b955bf84e
beataj <beata@emc2wave.com>
2022-02-14 20:59:44 -0800

    property binding

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -3,15 +3,15 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-
 })
 export class ProductsComponent {
   productName = 'A Book';
+  isDisabled = false;
 
   constructor() {
     setTimeout(() => {
-      this.productName = 'A Tree';
+      // this.productName = 'A Tree';
+      this.isDisabled = true;
     }, 3000);
   }
-
 }
