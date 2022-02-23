3cd9c41b04784c7fad6052417b7a143c2d4fb8ac
beataj <beata@emc2wave.com>
2022-02-22 19:20:50 -0800

    add onInit

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -1,17 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ProductsService } from "./products.service";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
 })
-export class ProductsComponent {
+export class ProductsComponent  implements OnInit {
   productName = 'A Book';
   isDisabled = true;
   products = ['']
 
   constructor(private productsService: ProductsService) {
-    this.products = this.productsService.getProducts();
+
     setTimeout(() => {
       // this.productName = 'A Tree';
       this.isDisabled = false;
@@ -30,6 +30,10 @@ export class ProductsComponent {
 
   onRemoveProduct(productName: string){
     this.products = this.products.filter(p => p !== productName)
+  }
+
+  ngOnInit(): void {
+      this.products = this.productsService.getProducts();
   }
 
 }
