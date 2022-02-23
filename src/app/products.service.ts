e270aede014b4d95ee5d4f32d410fe2e6aba44f0
beataj <beata@emc2wave.com>
2022-02-22 18:45:44 -0800

    create produts service

---
+++ src/app/products.service.ts
@@ -0,0 +1,12 @@
+export class ProductsService {
+  private products = ['A Book' ];
+
+  addProduct(productName: string) {
+    this.products.push(productName);
+  }
+
+  getProducts() {
+    return [...this.products];
+  }
+
+}
