a8f415cef2356d5a7d219509df2ea698af1d5d63
beataj <beata@emc2wave.com>
2022-02-22 21:26:58 -0800

    add routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ProductsComponent } from "./products.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
-  { path: 'produts', component: ProductsComponent }
+  { path: 'products', component: ProductsComponent }
 ]
 
 @NgModule({
